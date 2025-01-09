a5c32f374a11afd94040356c52ffad00831d0aaf
JuanJoseVillamizar <juanjvs1419@gmail.com>
2025-01-09 08:04:57 -0500

    feat(swagger): add SwaggerConfig for API documentation

--- src/main/java/JuanJose/ForoHub/controller/AuthController.java
+++ src/main/java/JuanJose/ForoHub/controller/AuthController.java
@@ -7,6 +7,11 @@ import JuanJose.ForoHub.dto.auth.AuthLoginRequest;
 import JuanJose.ForoHub.dto.auth.AuthResponse;
 import JuanJose.ForoHub.service.User.UserDetailsServiceImpl;
 import JuanJose.ForoHub.service.User.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +23,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name="Authentication", description = "Controller for authentication")
 public class AuthController {
 
 
@@ -30,10 +36,65 @@ public class AuthController {
     }
 
     @PostMapping("/login")
+    @Operation(
+            summary = "Login User",
+            description = "Authenticate a user and return the authentication token along with user details",
+            tags = {"Login,Authentication"},
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Authentication request with email and password",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AuthLoginRequest.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "User logged successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = AuthResponse.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<AuthResponse> login (@RequestBody @Valid AuthLoginRequest userRequest){
         return new ResponseEntity<>(userDetailsService.loginUser(userRequest), HttpStatus.OK);
     }
+
+
     @PostMapping("/signUp")
+    @Operation(
+            summary = "Sign up a new user",
+            description = "Register a new user and return the user details.",
+            tags = {"Sign Up,Authentication"},
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Request body with user details for registration",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CreateUserDTO.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "User created successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = DetailsResponseUserDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid input data",
+                            content = @Content(
+                                    mediaType = "application/json"
+                            )
+                    )
+            }
+    )
     public ResponseEntity<DetailsResponseUserDTO> singUp(@RequestBody @Valid CreateUserDTO data,
                                                              UriComponentsBuilder uriComponentsBuilder) {
         DetailsResponseUserDTO user = userService.createUser(data);
